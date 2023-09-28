ab7b406464feaadfe1a2f1decd93a6012a5e1ead
Umang Maheshwari <umang.ladha15@gmail.com>
2023-09-29 03:11:10 +0530

    fix: issue with updating of configurations

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -3,6 +3,7 @@ import { APP_PREFIX_PATH, AUTH_PREFIX_PATH } from "../configs/AppConfig";
 import AuthLayout from "../layouts/auth-layout";
 import ProductList from "../views/app-views/Products/ProductList";
 import EditProduct from "../views/app-views/Products/ProductEdit";
+import AddProduct from "../views/app-views/Products/ProductAdd";
 
 export const routes = [
   {
@@ -95,6 +96,16 @@ export const routes = [
         component: ProductList
       },
       {
+        path: `${APP_PREFIX_PATH}/configurations/add-config`,
+        exact: true,
+        meta: {
+          auth: true,
+          title: "Add Configuration",
+          breadcrumb: true,
+        },
+        component: AddProduct,
+      },
+      {
         path: `${APP_PREFIX_PATH}/configurations/:id`,
         exact: true,
         meta: {
@@ -104,16 +115,7 @@ export const routes = [
         },
         component: EditProduct
       },
-      {
-        path: `${APP_PREFIX_PATH}/products/add-product`,
-        exact: true,
-        meta: {
-          auth: true,
-          title: "Add Product",
-          breadcrumb: true,
-        },
-        component: lazy(() => import("../views/app-views/Products/ProductAdd")),
-      },
+      
       
       
 
