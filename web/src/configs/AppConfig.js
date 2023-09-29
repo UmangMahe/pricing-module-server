d2573a8fd2ab76c4ac8127d8f50182ec27e5068f
Umang Maheshwari <umang.ladha15@gmail.com>
2023-09-29 19:33:38 +0530

    fix: added route change

--- web/src/configs/AppConfig.js
+++ web/src/configs/AppConfig.js
@@ -3,6 +3,7 @@ import { DIR_LTR, NAV_TYPE_SIDE, SIDE_NAV_LIGHT } from '../constants/ThemeConsta
 import { env } from './EnvironmentConfig'
 
 export const APP_NAME = 'Pricing Module';
+export const APP_BASE_URL = import.meta.env.VITE_BASE_PATH
 export const API_BASE_URL = env.API_ENDPOINT_URL
 export const APP_PREFIX_PATH = '/dashboard';
 export const AUTH_PREFIX_PATH = '/auth';
